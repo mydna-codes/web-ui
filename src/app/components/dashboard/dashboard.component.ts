5e792a218fd8472a2e8e8d458f9a1784ad925bff
Nejc Jamnik <nj7267@student.uni-lj.si>
2021-01-03 14:49:23 +0100

    added chart.js and ng2-charts + basic dashboard design

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,5 +13,40 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  lineChartData: ChartDataSets[] = [
+    { data: [85, 72, 78, 75, 77, 75],},
+  ];
+
+  lineChartLabels: Label[] = ['', '', '', '', '', ''];
+
+  lineChartOptions = {
+    responsive: true,
+    scales: {
+      xAxes: [{
+        gridLines: {
+          display:false
+        }
+      }],
+      yAxes: [{
+        gridLines: {
+          display:false
+        },
+        ticks: {
+          display:false
+        }
+      }]
+    }
+  };
+
+  lineChartColors: Color[] = [
+    {
+      backgroundColor: '#673ab7',
+    },
+  ];
+
+  lineChartLegend = false;
+  lineChartPlugins = [];
+  lineChartType = 'line';
 
 }
