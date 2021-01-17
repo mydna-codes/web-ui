0c51f01df81c94bd9f5066bbd9465355b615f424
Nejc Jamnik <nj7267@student.uni-lj.si>
2021-01-17 23:18:22 +0100

    removed test from production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,15 @@
 export const environment = {
   production: true,
   plasmidJsSrc: 'https://plasmid-js.mydna.codes/',
-  backendUrl: 'https://sequence-bank-test.mydna.codes/v1/',
-  resultsUrl: "https://result-test.mydna.codes/v1/",
+  backendUrl: 'https://sequence-bank.mydna.codes/v1/',
+  resultsUrl: "https://result.mydna.codes/v1/",
   crudEndpoints: {
     dna: 'dna',
     enzyme: 'enzyme',
     gene: 'gene',
     results: 'results'
   },
-  graphQlEndpoint: 'https://analysis-test.mydna.codes/graphql',
+  graphQlEndpoint: 'https://analysis.mydna.codes/graphql',
   keycloak: {
     url: "https://keycloak.din-cloud.com/auth/",
     realm: "mydnacodes",
